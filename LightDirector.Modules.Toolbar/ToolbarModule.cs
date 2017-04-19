2ef5cf7df2c5df82c41f40d2646536e0cfeebaeb
binton10186 <jdbanton@googlemail.com>
2017-04-20 00:21:47 +0100

    Add files via upload

---
+++ LightDirector.Modules.Toolbar/ToolbarModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using LightDirector.Infrastructure;
+using LightDirector.Modules.Toolbar.Views;
+using Prism.Regions;
+
+namespace LightDirector.Modules.Toolbar
+{
+   public class ToolbarModule : Module, Prism.Modularity.IModule
+   {
+      private readonly IRegionManager _regionManager;
+
+      public ToolbarModule(IRegionManager regionManager)
+      {
+         _regionManager = regionManager;
+      }
+
+      public void Initialize()
+      {
+         _regionManager.RegisterViewWithRegion(RegionNames.Toolbar, typeof(ToolbarView));
+      }
+   }
+}
---
+++ LightDirector.Modules.Toolbar/ToolbarModuleRegistry.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightDirector.Modules.Toolbar
+{
+   public class ToolbarModuleRegistry : Module
+   {
+      protected override void Load(ContainerBuilder builder)
+      {
+         builder.RegisterType<ToolbarModule>();
+      }
+   }
+}
